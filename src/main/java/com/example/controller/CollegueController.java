2c00296d74c1e816a5f8cc76cad228698f3f4509
Cecile Peyras <peyrascecile@gmail.com>
2019-08-29 10:28:24 +0200

    Recherche par matricule

--- src/main/java/com/example/controller/CollegueController.java
+++ src/main/java/com/example/controller/CollegueController.java
@@ -6,6 +6,9 @@ package com.example.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entite.Collegue;
+import com.example.exception.CollegueNonTrouveException;
 import com.example.service.CollegueService;
 
 /**
@@ -23,24 +27,38 @@ import com.example.service.CollegueService;
 @RestController
 public class CollegueController {
 
-	 @GetMapping(value = "/collegues")
-	  public List<String> findClient(@RequestParam String nom) {
-	        
-		 CollegueService colService = new CollegueService(); 
-		 
-		 List<Collegue> listeCollegue = colService.rechercherParNom(nom); 
-		 
-		 List<String> listeMatricule = new ArrayList<String>(); 
-		 
-		 for (Collegue liste : listeCollegue) {
-			 listeMatricule.add(liste.getMatricule()); 
-			 
-		 }
-		 
-		 return listeMatricule; 
-		 
-	
-	 }
-	
-	
+	CollegueService colService = new CollegueService();
+
+	@GetMapping(value = "/collegues")
+	public List<String> findByNom(@RequestParam String nom) {
+
+		List<Collegue> listeCollegue = colService.rechercherParNom(nom);
+
+		List<String> listeMatricule = new ArrayList<String>();
+
+		for (Collegue liste : listeCollegue) {
+			listeMatricule.add(liste.getMatricule());
+
+		}
+
+		return listeMatricule;
+
+	}
+
+	@GetMapping(value = "/collegues/{matricule}")
+	public Collegue findByMatricule(@PathVariable String matricule) {
+
+		Collegue collegue = colService.rechercherParMatricule(matricule);
+
+		return collegue;
+
+	}
+
+	@ExceptionHandler({ CollegueNonTrouveException.class })
+	public ResponseEntity<String> error() {
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Collègue non trouvé");
+
+	}
+
 }
---
+++ src/main/java/com/example/exception/CollegueNonTrouveException.java
@@ -0,0 +1,15 @@
+package com.example.exception;
+
+public class CollegueNonTrouveException extends RuntimeException {
+	
+	/**
+	 * Constructeur
+	 * 
+	 * @param message
+	 */
+	public CollegueNonTrouveException(String message) {
+		super(message);
+
+	}
+
+}
